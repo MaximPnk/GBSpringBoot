a114e8c5e4ed00c92e54223b6d0c8c4314a0fbf3
MaximPnk <adm.mjkee@yandex.ru>
2020-12-15 19:50:01 +0300

    done

---
+++ src/main/java/ru/pankov/entity/Product.java
@@ -0,0 +1,27 @@
+package ru.pankov.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    private long id;
+    private String title;
+    private BigDecimal price;
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
